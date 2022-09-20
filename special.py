1619a320d6365f20622e289db28ccce929cb392c
KngSystems <eddyivan@hotmail.com>
2022-09-20 17:26:15 -0500

    demo version 1 deteccion split by spaces

--- special.py
+++ special.py
@@ -1,14 +1,15 @@
 import re
+import sys
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
 from datetime import date
+from io import open
 
 def specialCharacters(args):
-    pattern = re.compile('[^A-Z0-9_.,Ñ/-]')
+    pattern = re.compile('[^A-Z0-9_.,Ñ/ ;-]')
     res=pattern.search(args)==None
     return res
-
 
 today=date.today();
 Tk().withdraw
@@ -18,7 +19,7 @@ print(filename)
 print(filenamesave)
 contador=0
 path=filenamesave+"/"+str(today)+"_bitacora.txt"
-text=open(path,'w')
+text=open(path,mode='w')
 
 
 with open(filename) as archivo:
@@ -26,8 +27,20 @@ with open(filename) as archivo:
         contador+=1
         res=specialCharacters(linea.strip())
         if (res==False):
-            text.write(f'Caracter especial en linea: {contador} \n')
             print(f'Caracter especial en linea: {contador}')
+            text.write(f'Caracter especial en linea: {contador} \n')
+            sep=linea.split(";")
+            for campo in sep:
+                f=specialCharacters(campo.strip())
+                if (f==False):
+                    z=campo.split()
+
+                    text.write(f'{campo} \n')
+                    for i in z:
+                        r=specialCharacters(i)
+                        if(r==False):
+                            text.write(f'error-->{i}<-- \n')
+                            print(f'error-->{i}<--')
 
 text.close()
 
